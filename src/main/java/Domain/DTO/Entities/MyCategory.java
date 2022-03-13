f1f76e75235ccbab6c2dda19f7bd68ea1de8c351
Elias Ojeda A <metall65eli@gmail.com>
2022-03-13 19:25:01 -0300

    A dto class which contains the "category" description for the database

---
+++ src/main/java/Domain/DTO/Entities/MyCategory.java
@@ -0,0 +1,32 @@
+package Domain.DTO.Entities;
+
+public class MyCategory {
+
+	private int id;
+	private String title;
+	private String description;
+
+	 public int getId() {
+		  return id;
+	 }
+
+	 public void setId(int id) {
+		  this.id = id;
+	 }
+
+	 public String getTitle() {
+		  return title;
+	 }
+
+	 public void setTitle(String title) {
+		  this.title = title;
+	 }
+
+	 public String getDescription() {
+		  return description;
+	 }
+
+	 public void setDescription(String description) {
+		  this.description = description;
+	 }
+}
