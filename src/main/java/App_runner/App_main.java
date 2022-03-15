c099a14cef9a76010c5df67fc36a522115aa8d63
Elias Ojeda A <metall65eli@gmail.com>
2022-03-15 19:43:46 -0300

    some edits of the testing

--- src/main/java/App_runner/App_main.java
+++ src/main/java/App_runner/App_main.java
@@ -1,6 +1,40 @@
 package App_runner;
-public class App_main {
+
+import Domain.DTO.ConfigurationEntity.Configuration;
+import Domain.DTO.Entities.MyFile;
+import Domain.FileManagement.MyFolder;
+import Infrastructure.Persistance.Configuration.ConfigurationManager;
+import Presentation.mainwindow.MainWindowController;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App_main extends Application {
 	 public static void main(String[] args){
-		//nothing written here yet
+
+	 }
+
+	 @Override
+	 public void start(Stage stage) throws Exception {
+		  /**Platform.runLater(()->{
+		  	 try {
+				  Stage st = new Stage();
+				  MainWindowController main = new MainWindowController();
+				  FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI_FX/MainWindow/main_window_fx.fxml"));
+				  loader.setController(main);
+				  st.setScene(new Scene(loader.load()));
+				  st.show();
+			 }catch (IOException e) {
+				  e.printStackTrace();
+			 }
+
+		  });**/
+
+
+
 	 }
 }
