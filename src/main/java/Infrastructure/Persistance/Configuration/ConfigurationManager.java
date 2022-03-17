d1b16dc3257b77bdc45cf3012876981c200ebd40
Elias Ojeda A <metall65eli@gmail.com>
2022-03-16 21:32:12 -0300

    Documentation of the class and its method have been provided.

--- src/main/java/Infrastructure/Persistance/Configuration/ConfigurationManager.java
+++ src/main/java/Infrastructure/Persistance/Configuration/ConfigurationManager.java
@@ -9,17 +9,30 @@ import java.io.FileOutputStream;
 import java.util.Objects;
 import java.util.Properties;
 
+/**
+ * If we want to write and read a file where the configuration. ConfigurationManager class should be instanced.
+ * it's been built under the singleton pattern. which suits very well for this purpose of saving and reading the configuration file.
+ *
+ */
 public class ConfigurationManager {
-
 
 	 private static ConfigurationManager conf_manager = null;
 	 private Configuration currentconfiguration;
 	 private String root;
+
+	 /**
+	  * A private constructor for designing the singleton
+	  */
 	 private ConfigurationManager() {
 			this.currentconfiguration = new Configuration();
 			this.root = MyFolder.getDriveRoots()[0].getAbsolutePath();
 	 }
 
+
+	 /**
+	  * when we want to instance this class, we should call this static method which will instance the class.
+	  * @return the instance of this class.
+	  */
 	 public static ConfigurationManager getInstance(){
 	 	 if(Objects.isNull(conf_manager)){
 	 	 	 conf_manager = new ConfigurationManager();
@@ -28,11 +41,17 @@ public class ConfigurationManager {
 	 }
 
 
+	 /**
+	  *
+	  * when we have already instanced this class and we want to save a configuration file,
+	  * we should call this method, which requires a Configuration object for the saving process.
+	  * @param conf the configuration object needed for the saving method to work.
+	  * @return false if the configuration wasn't saved or true if it was saved
+	  */
 	 public boolean saveConfiguration(Configuration conf){
 	 	 boolean issaved = false;
 		if(!Objects.isNull(conf)){
 			 try {
-
 				  File conf_path = new File(root + Configuration.FULL_CONF_LOCATION);
 				  FileOutputStream output = new FileOutputStream(conf_path);
 				  Properties prop = new Properties();
@@ -55,6 +74,11 @@ public class ConfigurationManager {
 	 }
 
 
+	 /**
+	  * when we have already instanced this class and we want to get the configuration loaded or to load the configuration,
+	  * we call this method which returns a Configuration reference where the configuration is loaded.
+	  * @return a configuration object.
+	  */
 	 public Configuration getConfiguration(){
 			  try {
 				   File conf_path = new File(root + Configuration.FULL_CONF_LOCATION);
