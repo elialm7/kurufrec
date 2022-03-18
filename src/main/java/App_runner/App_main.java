9ba75461035a7cdbb2b43931fb1bfdf756a55a2b
Elias Ojeda A <metall65eli@gmail.com>
2022-03-18 17:28:20 -0300

    javafx launcher removed

--- src/main/java/App_runner/App_main.java
+++ src/main/java/App_runner/App_main.java
@@ -1,40 +1,8 @@
 package App_runner;
 
-import Domain.DTO.ConfigurationEntity.Configuration;
-import Domain.DTO.Entities.MyFile;
-import Domain.FileManagement.MyFolder;
-import Infrastructure.Persistance.Configuration.ConfigurationManager;
-import Presentation.mainwindow.MainWindowController;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-public class App_main extends Application {
+public class App_main{
 	 public static void main(String[] args){
 
 	 }
 
-	 @Override
-	 public void start(Stage stage) throws Exception {
-		  /**Platform.runLater(()->{
-		  	 try {
-				  Stage st = new Stage();
-				  MainWindowController main = new MainWindowController();
-				  FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI_FX/MainWindow/main_window_fx.fxml"));
-				  loader.setController(main);
-				  st.setScene(new Scene(loader.load()));
-				  st.show();
-			 }catch (IOException e) {
-				  e.printStackTrace();
-			 }
-
-		  });**/
-
-
-
-	 }
 }
