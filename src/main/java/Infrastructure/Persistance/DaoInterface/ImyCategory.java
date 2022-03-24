c0d3eeb4e798185c60bc32ffebd9ee2f6431c959
Elias Ojeda A <metall65eli@gmail.com>
2022-03-24 11:59:07 -0300

    interface for mycategory

---
+++ src/main/java/Infrastructure/Persistance/DaoInterface/ImyCategory.java
@@ -0,0 +1,7 @@
+package Infrastructure.Persistance.DaoInterface;
+
+import Domain.DTO.Entities.MyCategory;
+
+public interface ImyCategory extends Dao<MyCategory> {
+	//methods that belong only to the category goes here
+}
