dda9a010c0c23a43735a6414dad16ff531ee5bcc
Elias Ojeda A <metall65eli@gmail.com>
2022-03-24 12:00:36 -0300

    interface for the myfile only

---
+++ src/main/java/Infrastructure/Persistance/DaoInterface/ImyFile.java
@@ -0,0 +1,8 @@
+package Infrastructure.Persistance.DaoInterface;
+
+import Domain.DTO.Entities.MyFile;
+
+public interface ImyFile extends Dao<MyFile> {
+	 //methods that belong only to myfile goes here
+
+}
