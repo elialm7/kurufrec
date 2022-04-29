b70d71b3b85a0b162ca8d5224481840bba9bd7c2
Elias Ojeda A <metall65eli@gmail.com>
2022-04-29 14:00:03 -0400

    Javafx has been implemented to set up and run the UI.

--- src/main/java/core/App_runner/App_main.java
+++ src/main/java/core/App_runner/App_main.java
@@ -1,13 +1,39 @@
 package core.App_runner;
 
 
+import core.controller.MainWinController;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 
-public class App_main{
+import java.io.IOException;
+
+public class App_main extends Application {
+
 	 private static Logger log = Logger.getLogger(App_main.class);
+
+
 	 public static void main(String[] args){
-
-
+	 	 launch(args);
 	 }
 
+	 @Override
+	 public void start(Stage stage) throws Exception {
+		  Platform.runLater(() -> {
+			   try {
+			   FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/windows/main_fx.fxml"));
+			   MainWinController controller = new MainWinController(stage);
+			   fxmlLoader.setController(controller);
+			   stage.setScene(new Scene(fxmlLoader.load()));
+			   stage.setTitle("KuruFrec tool alpha");
+			   stage.setResizable(false);
+			   stage.show();
+			   } catch (IOException e) {
+					log.debug(e);
+			   }
+		  });
+	 }
 }
