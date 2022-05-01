76474c39b02e3fcaac691a21627591b773c76abc
Elias Ojeda A <metall65eli@gmail.com>
2022-04-30 21:13:23 -0400

    edited the test method

--- src/test/java/MyFolderTest/myfolderclass_test.java
+++ src/test/java/MyFolderTest/myfolderclass_test.java
@@ -3,6 +3,7 @@ package MyFolderTest;
 import core.model.FileManagement.MyFolder;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
 
 public class myfolderclass_test {
@@ -12,8 +13,8 @@ public class myfolderclass_test {
 	 @Test
 	 public void testinglistingFiles(){
 
-		  MyFolder folder = new MyFolder();
-		  List<String> fileslist = folder.getFilesList("C:\\MyFiles\\");
+		  MyFolder folder = new MyFolder(new File("C:\\MyFiles\\"));
+		  List<String> fileslist = folder.getFilesList();
 		  int counter = 0;
 		  for(String a:  fileslist){
 			   System.out.println((counter+1) +" -  "+ a);
