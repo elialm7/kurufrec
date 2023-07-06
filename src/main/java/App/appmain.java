8d2639c6a565ae3e6b8f315e143103ca5b6c3830
Elias Ojeda A <metall65eli@gmail.com>
2023-07-06 14:20:07 -0400

    code for running the UI implemented

--- src/main/java/App/appmain.java
+++ src/main/java/App/appmain.java
@@ -5,12 +5,16 @@
  */
 package App;
 
-import model.Folder.MyFolder;
-import model.Lexicon.JapaneseLexicon.JpFrecuencier.JpFrecuencier;
-import model.Lexicon.JapaneseLexicon.JpWord.JpWord;
-import model.Lexicon.JapaneseLexicon.JpWord.JpWordBuilder;
-import model.Property.ConfProperty;
-import model.Property.PropertyLoader;
+import Controller.MainWindowController;
+import Model.Folder.MyFolder;
+import Model.Lexicon.JapaneseLexicon.JpKuroFrecuencier.JpFrecuencier;
+import Model.Lexicon.JapaneseLexicon.JpWord.JpWord;
+import Model.Lexicon.JapaneseLexicon.JpWord.JpWordBuilder;
+import Model.Property.ConfProperty;
+import Model.Property.PropertyLoader;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -19,108 +23,37 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class appmain {
-	 private static String propertyFile = "..\\conf.properties";
-	 private static PropertyLoader loader;
-	 private static ConfProperty properties;
-	 private static File jar = MyFolder.getJarFile();
+public class appmain extends Application {
+	 private Stage mainwindowStage;
 	 public static void main(String args[]){
-	 	 start();
+		launch(args);
 	 }
-
-	 private static void start(){
+	 @Override
+	 public void start(Stage stage) throws Exception {
+		this.mainwindowStage = stage;
+		this.StartMainWindow();
+	 }
+	 private void StartMainWindow(){
 		  try {
-			   File fileT = new File(MyFolder.getJarFile(), propertyFile);
-			   System.out.println("#### Welcome to KuruFrec 1.1 #### ");
-			   System.out.println("Verifying if the file exists...");
-			   loader = new PropertyLoader(propertyFile);
-			   if(!fileT.exists()){
-					System.out.println("Creating the conf template in the same folder. please set it up!!!.");
-					loader.createDefaulProperty();
-			   }else {
-					properties = loader.loadproperty();
-					if (!isvalid(properties)) {
-						 System.out.println("The file property is not valid. please check your wfile and wfolder. Only one should be true. and the locations should be filled properly.");
-						 return;
-					}
-					if (!isValidData(properties)) {
-						 System.out.println("The data where the fields point at are not valid. or cannot be accessed. please make sure they are valid or exists.");
-						 return;
-					}
-					System.out.println("Making the frecuency list...");
-					makeFrecuency();
-					System.out.println("The frecuency list is done. it is available at :" + properties.getOutput());
-					System.out.println("Author: Rodolfo Elias Ojeda almada.");
-					System.out.println("Code hosted on: https://github.com/elikawa7");
-			   }
+		  		FXMLLoader mainwindowloader = new FXMLLoader(getClass().getResource("/main/mainwindow.fxml"));
+		  		MainWindowController windowController = new MainWindowController(this.mainwindowStage, mainwindowloader);
+			    windowController.startwindow();
 		  } catch (IOException e) {
 			   e.printStackTrace();
+			   //some sort of logging must be implemented
 		  }
 	 }
 
-	 private static boolean isvalid(ConfProperty prop){
-	 	 if(prop.isWfile() && prop.isWfolder()){
-	 	 	 return false;
-		 }
-		 if(!prop.isWfile() && !prop.isWfolder()){
-	 	 	 return false;
-		 }
-		 if(prop.getFolderloc().isEmpty() && prop.getFileloc().isEmpty()&&prop.getOutput().isEmpty()){
-	 	 	 return false;
-		 }
-		 if(prop.isWfolder() && prop.getFolderloc().isEmpty()){
-	 	 	 return false;
-		 }
-		 if(prop.isWfile() && prop.getFileloc().isEmpty()){
-	 	 	 return false;
-		 }
-	 	 return true;
-	 }
 
-
-	 private static boolean isValidData(ConfProperty prop){
-		  File file;
-	 	 if(prop.isFromjar()){
-	 	 	 if(prop.isWfile()){
-	 	 	 		file =  new File(jar, prop.getFileloc());
-	 	 	 		return file.exists();
-			 }
-			 if(prop.isWfolder()){
-	 	 	 	 file = new File(jar, prop.getFolderloc());
-	 	 	 	 return file.isDirectory();
-			 }
-			 file = new File(jar, prop.getOutput());
-	 	 	 if(file.exists()){
-	 	 	 	 return true;
-			 }
-
-		 }else{
-				if(prop.isWfile()){
-					 file = new File(prop.getFileloc());
-					 return file.exists();
-				}
-				if(prop.isWfolder()){
-					 file = new File(prop.getFolderloc());
-					 return file.isDirectory();
-				}
-				file = new File(prop.getOutput());
-				if(file.exists()){
-					 return true;
-				}
-		 }
-
-		 return false;
-	 }
+/*
 
 
 	 private static void makeFrecuency(){
 		  JpFrecuencier jpFrecuencier = getFrecuencierInstance();
 		  List<JpWord> frecuencyWordList = jpFrecuencier.getFrecuencyWordList();
-		  Predicate<JpWord> bytype = jpWord -> {
-			   return jpWord.getType().equalsIgnoreCase(JpWordBuilder.ADVERB_ENGLISH) || jpWord.getType().
-					   equalsIgnoreCase( JpWordBuilder.NOUN_ENGLISH) || jpWord.getType().equalsIgnoreCase(JpWordBuilder.VERB_ENGLISH) || jpWord.getType().
-					   equalsIgnoreCase(JpWordBuilder.ADJECTIVE_ENGLISH);
-		  };
+		  Predicate<JpWord> bytype = jpWord -> jpWord.getType().equalsIgnoreCase(JpWordBuilder.ADVERB_ENGLISH) || jpWord.getType().
+				 equalsIgnoreCase( JpWordBuilder.NOUN_ENGLISH) || jpWord.getType().equalsIgnoreCase(JpWordBuilder.VERB_ENGLISH) || jpWord.getType().
+				 equalsIgnoreCase(JpWordBuilder.ADJECTIVE_ENGLISH);
 		  List<JpWord> frecuencyResults = frecuencyWordList.stream().filter(bytype).collect(Collectors.toList());
 		  File output = getFile();
 		  writeintofile(output, frecuencyResults);
@@ -172,6 +105,7 @@ public class appmain {
 			   e.printStackTrace();
 		  }
 	 }
+*/
 
 }
 
