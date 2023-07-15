f2b732144b4a08d8ec77d617689fc7bf3c01f998
Elias Ojeda A <metall65eli@gmail.com>
2023-07-15 18:16:36 -0400

    added some logs

--- src/main/java/App/AppMainLauncher.java
+++ src/main/java/App/AppMainLauncher.java
@@ -1,8 +1,15 @@
 package App;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AppMainLauncher {
 
+	 private static Logger AppMainLauncherLogger = LogManager.getLogger(AppMainLauncher.class);
 	 public static void main(String[] args){
-	 	Appmain.main(args);
+		  AppMainLauncherLogger.info("Appmain is being called...");
+		  Appmain.main(args);
 	 }
+
 }
