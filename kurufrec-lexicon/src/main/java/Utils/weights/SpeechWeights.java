b8d6adfbd089f6d43a825c7e3c0290801421e464
elikawa7 <metall65eli@gmail.com>
2023-12-04 23:44:44 -0300

    weights for the words

---
+++ kurufrec-lexicon/src/main/java/Utils/weights/SpeechWeights.java
@@ -0,0 +1,19 @@
+package Utils.weights;
+
+
+//weights are used for sorting.
+//these are just arbitrary numbers. it's not based on anything...
+public class SpeechWeights {
+    public static final int NOUN_WEIGH = 0;
+    public static final int VERB_WEIGH = 1;
+    public static final int ADJECTIVE_WEIGH = 2;
+    public static final int PARTICLE_WEIGH = 3;
+    public static final int ADVERB_WEIGH = 4;
+    public static final int AUXILIARY_VERB_WEIGH = 5;
+    public static final int SYMBOL_WEIGH = 6;
+    public static final int ADNOMINAL_ADJECTIVE_WEIGH = 7;
+    public static final int INTERJECTION_WEIGH = 8;
+    public static final int PREFIX_WEIGH = 9;
+    public static final int CONJUNCTION_WEIGH = 10;
+    public static final int NO_DEFINED_WEIGH = -1;
+}
