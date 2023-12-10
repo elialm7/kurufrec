3de0ad5f3475f62cadf94301b1b017fd8177936a
elikawa7 <metall65eli@gmail.com>
2023-12-10 02:17:02 -0300

    reader and writer abstract classes for input and output.

---
+++ kurufrec-io/src/main/java/FileIO/in/KuruReader.java
@@ -0,0 +1,16 @@
+package FileIO.in;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public abstract class KuruReader<T> {
+
+    protected Charset charset = StandardCharsets.UTF_8;
+
+    public void setCharSet(Charset charset){
+        this.charset = charset;
+    }
+
+   public abstract T readContent() throws IOException;
+}
---
+++ kurufrec-io/src/main/java/FileIO/out/KuruWriter.java
@@ -0,0 +1,17 @@
+package FileIO.out;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public abstract class KuruWriter<T> {
+
+    protected Charset charset = StandardCharsets.UTF_8;
+
+    public void setCharset(Charset charset){
+        this.charset = charset;
+    }
+
+    public abstract void writeContent(T input) throws IOException;
+
+}
