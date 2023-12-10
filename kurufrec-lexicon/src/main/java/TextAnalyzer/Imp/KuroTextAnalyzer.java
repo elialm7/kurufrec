961debb174cfdf1b012f1a186f0462541a1c718d
elikawa7 <metall65eli@gmail.com>
2023-12-10 02:15:08 -0300

    implementation of the textanalyzer using the kuromoji library

--- kurufrec-lexicon/src/main/java/TextAnalyzer/Imp/KuroTextAnalyzer.java
+++ kurufrec-lexicon/src/main/java/TextAnalyzer/Imp/KuroTextAnalyzer.java
@@ -1,21 +1,38 @@
 package TextAnalyzer.Imp;
 
+import KanaConversion.KanaConversion;
 import TextAnalyzer.TextAnalyzer;
+import TextEntities.Word.Word;
+import Utils.kuromoji.KuroMojiFunctionsUtils;
+import com.atilika.kuromoji.TokenizerBase;
 import com.atilika.kuromoji.ipadic.Token;
 import com.atilika.kuromoji.ipadic.Tokenizer;
 
 import java.util.List;
 
-public class KuroTextAnalyzer implements TextAnalyzer<Token> {
+public class KuroTextAnalyzer implements TextAnalyzer<Word> {
 
     private String inputText;
+    private KanaConversion converter;
 
-    KuroTextAnalyzer(String inputText){
+    private boolean searchmode = false;
+    private boolean extendedmode = false;
+
+     KuroTextAnalyzer(String inputText, KanaConversion converter){
         this.inputText = inputText;
+        this.converter = converter;
     }
+
+
+    private Tokenizer defaultTokenizer(){
+         return new Tokenizer();
+    }
+
     @Override
-    public List<Token> analyze() {
-        Tokenizer tokenizer = new Tokenizer();
-        return tokenizer.tokenize(inputText);
+    public List<Word> analyze() {
+         if(converter == null) throw new IllegalArgumentException("The Kanaconverter for the readings is not provided. Provide a KanaConversion instance.");
+
+        Tokenizer tokenizer = defaultTokenizer();
+        return KuroMojiFunctionsUtils.NewFunctionUtils(converter).fromTokens(tokenizer.tokenize(inputText));
     }
 }
