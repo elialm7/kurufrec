c067e2a6e7d082d8abc171cd4e2171e81d568d1a
elikawa7 <metall65eli@gmail.com>
2023-12-25 18:15:37 -0300

    splitted big method into smaller functions that performs one responsability only.

--- kurufrec-lexicon/src/main/java/TextFrecuencier/WordFrecuencier/WordFrecuencier.java
+++ kurufrec-lexicon/src/main/java/TextFrecuencier/WordFrecuencier/WordFrecuencier.java
@@ -20,25 +20,32 @@ public class WordFrecuencier implements Frecuencier<Word> {
         this.frecuencies = new HashMap<>();
     }
 
+    private void addWordtoMap(List<Word> wordListreference){
+         for(Word currentWord: wordListreference){
+             if(frecuencies.containsKey(currentWord)){
+                 int frecuency = frecuencies.get(currentWord);
+                 frecuencies.put(currentWord, frecuency+1);
+                 continue;
+             }
+             frecuencies.put(currentWord, 1);
+         }
+    }
+
+    private void createResultList(List<Word> resultWordListReference){
+         for(Word currentWord: frecuencies.keySet()){
+             int wordFrecuency = frecuencies.get(currentWord);
+             currentWord.setFrecuency(wordFrecuency);
+             resultWordListReference.add(currentWord);
+         }
+    }
+
     @Override
     public List<Word> doFrecuency() {
          if(analyzer == null) throw new IllegalArgumentException("The TextAnalyzer is not provided. Provide a TextAnalyzer instance. ");
-
         List<Word> words = analyzer.analyze();
-        for(Word eword: words){
-            if(frecuencies.containsKey(eword)){
-                int frecuency = frecuencies.get(eword);
-                frecuencies.put(eword, frecuency+1);
-                continue;
-            }
-            frecuencies.put(eword, 1);
-        }
+        addWordtoMap(words);
         List<Word> wordsresults = new ArrayList<>();
-        for(Word w: frecuencies.keySet()){
-            int wordFrecuency = frecuencies.get(w);
-            w.setFrecuency(wordFrecuency);
-            wordsresults.add(w);
-        }
+        createResultList(wordsresults);
         return wordsresults;
     }
 }
