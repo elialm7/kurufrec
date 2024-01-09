9f363238b4e63a45619b09cfcdf5598d82bb8a6c
elikawa7 <metall65eli@gmail.com>
2024-01-09 16:11:33 -0300

    chore: added log4 dependency
    chore: configuration file set up.
    
    chore: testing log.info on main.
    
    chore: logging added.

--- kurufrec-gui/src/main/java/app/MainApp.java
+++ kurufrec-gui/src/main/java/app/MainApp.java
@@ -4,16 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MainApp extends Application{
 
-
+    private static Logger log = LogManager.getLogger(MainApp.class);
     public static void main(String[] args){
     launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        log.info("Starting controller and loading fxmls...");
         MainViewController controller = new MainViewController();
         controller.setMainViewStage(stage);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxmlViews/kurufrecmainviev.fxml"));
