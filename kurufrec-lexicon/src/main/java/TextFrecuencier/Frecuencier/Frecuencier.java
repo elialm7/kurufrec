e1a032987ed323dced3f7686f33d8a2c75eff4fc
elikawa7 <metall65eli@gmail.com>
2024-01-28 23:38:52 -0300

    chore: class renamed and better name for events and listener.

--- kurufrec-lexicon/src/main/java/TextFrecuencier/Frecuencier/Frecuencier.java
+++ kurufrec-lexicon/src/main/java/TextFrecuencier/Frecuencier/Frecuencier.java
@@ -1,15 +1,59 @@
 package TextFrecuencier.Frecuencier;
-import java.util.List;
-import TextFrecuencier.FrecuencyStrategy.FrecuencyStrategy;
 
-public class Frecuencier<T>{
-    private FrecuencyStrategy<T> strategy;
-    public Frecuencier(){}
-    public void setStrategy(FrecuencyStrategy<T> strategy){
-        this.strategy = strategy;
+import TextFrecuencier.FrecuencierListener.FrecuencierListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public abstract class Frecuencier<T>{
+
+
+    public enum Event{
+        PREPARING,READY,EXECUTING,FAILED,FINISHING, FINISHED
     }
-    public List<T> doFrecuency(){
-        if(strategy == null) throw new NullPointerException("Strategy is not defined");
-        return strategy.executeStrategy();
+
+    private List<FrecuencierListener> listeners = new ArrayList<>();
+    private Event evt;
+    private int min = 0;
+    private int current = 0;
+    private int max = 0;
+    public  void  addListener(FrecuencierListener observer){
+        if(listeners.contains(observer))return;
+        this.listeners.add(observer);
     }
+    public void removeListener(FrecuencierListener observer){
+        if(!listeners.contains(observer))return;
+        listeners.remove(observer);
+    }
+
+    protected void emitEvent(Event evt, Object origin){
+        this.evt = evt;
+        if(listeners.isEmpty()) return;
+        listeners.forEach(frecuencierListener -> frecuencierListener.handleEvent(evt, origin));
+    }
+    protected void setCurrent(int current){
+        this.current = current;
+    }
+    protected void setMax(int max){
+        this.max = max;
+    }
+    protected void setMin(int min){
+        this.min = min;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public abstract T execute();
+
 }
