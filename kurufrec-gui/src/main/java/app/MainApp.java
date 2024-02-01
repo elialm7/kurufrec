53ab8420474b076e995f81bbc4338091ae373b49
elikawa7 <metall65eli@gmail.com>
2024-01-31 22:06:13 -0300

    chore: more rewriting.

--- kurufrec-gui/src/main/java/app/MainApp.java
+++ kurufrec-gui/src/main/java/app/MainApp.java
@@ -1,13 +1,7 @@
 package app;
-import Interactor.MainViewInteractor;
 import View.MainView;
-import controller.MainViewController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class MainApp extends Application{
     public static void main(String[] args){
