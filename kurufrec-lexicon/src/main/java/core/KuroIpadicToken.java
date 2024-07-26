9b53cb23e9451595f15b7204d76320d5b19ec91a
eli _ <metall65eli@gmail.com>
2024-07-26 18:04:05 -0400

    Not needed boilerplate, records have this already implemented

--- kurufrec-lexicon/src/main/java/core/KuroIpadicToken.java
+++ kurufrec-lexicon/src/main/java/core/KuroIpadicToken.java
@@ -23,16 +23,4 @@ public record KuroIpadicToken(String surface,
         );
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        KuroIpadicToken that = (KuroIpadicToken) o;
-        return Objects.equals(surface, that.surface) && Objects.equals(reading, that.reading) && Objects.equals(baseform, that.baseform) && Objects.equals(speechpart, that.speechpart) && Objects.equals(pronunciation, that.pronunciation) && Objects.equals(conjugationForm, that.conjugationForm) && Objects.equals(conjugationType, that.conjugationType);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(surface, speechpart, baseform, conjugationForm, conjugationType, reading, pronunciation);
-    }
 }
